33cf9547013ed39f9bf18031dce3d0b8b48de986
Matt Andre <mandre@colocrossing.com>
2014-08-28 17:34:31 -0400

    Add getDevice call from Subnet Object

--- src/ColoCrossing/Object/Subnet.php
+++ src/ColoCrossing/Object/Subnet.php
@@ -2,8 +2,19 @@
 
 class ColoCrossing_Object_Subnet extends ColoCrossing_Resource_Object
 {
+
+	public function getNetwork()
+	{
+		$client = $this->getClient();
+
+		return $this->getObject('network');
+	}
+
 	public function getDevice()
 	{
-		return 'Device #' . $this->getValue('device')['id'];
+		$client = $this->getClient();
+
+		return $this->getObject('device', $client->devices);
 	}
+
 }
