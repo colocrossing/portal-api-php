33cfc4e8fd13717aad1dce8d1775c259a46ae3af
Matt Andre <mandre@colocrossing.com>
2014-08-29 10:11:33 -0400

    Retrieve Network From Subnet Object. Return Actual Network if Owned by Client Otherwise Return Generic Object

--- src/ColoCrossing/Object/Subnet.php
+++ src/ColoCrossing/Object/Subnet.php
@@ -6,8 +6,16 @@ class ColoCrossing_Object_Subnet extends ColoCrossing_Resource_Object
 	public function getNetwork()
 	{
 		$client = $this->getClient();
+		$network = $this->getValue('network');
 
-		return $this->getObject('network');
+		if(empty($network) || !is_array($network))
+		{
+			return null;
+		}
+
+		$resource = isset($network['owner']) && is_array($network['owner']) ? $client->networks : null;
+
+		return $this->getObject('network', $resource);
 	}
 
 	public function getDevice()
