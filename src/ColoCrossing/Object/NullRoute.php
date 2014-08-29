18f1585a5a9d5cb800393ebb50ca6e2c980e407c
Matt Andre <mandre@colocrossing.com>
2014-08-29 10:26:58 -0400

    Fill in Null Route Object

--- src/ColoCrossing/Object/NullRoute.php
+++ src/ColoCrossing/Object/NullRoute.php
@@ -3,4 +3,25 @@
 class ColoCrossing_Object_NullRoute extends ColoCrossing_Resource_Object
 {
 
+	public function getDateAdded()
+	{
+		$date_added = $this->getValue('date_added');
+
+		return $date_added && isset($date_added) ? strtotime($date_added) : null;
+	}
+
+	public function getDateExpire()
+	{
+		$date_expire = $this->getValue('date_expire');
+
+		return $date_expire && isset($date_expire) ? strtotime($date_expire) : null;
+	}
+
+	public function getSubnet()
+	{
+		$client = $this->getClient();
+
+		return $this->getObject('subnet', $client->subnets);
+	}
+
 }
