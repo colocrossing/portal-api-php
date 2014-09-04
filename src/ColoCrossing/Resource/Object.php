a4a9cb8b2290c0d88e24698b45a065a528103e3e
Matt Andre <mandre@colocrossing.com>
2014-09-04 13:53:31 -0400

    Change Methods to Protected

--- src/ColoCrossing/Resource/Object.php
+++ src/ColoCrossing/Resource/Object.php
@@ -17,7 +17,7 @@ class ColoCrossing_Resource_Object extends ColoCrossing_Object
 		return $this->resource;
 	}
 
-	public function getResourceChildCollection($child_type, array $options = null)
+	protected function getResourceChildCollection($child_type, array $options = null)
 	{
 		$parent_id = $this->getId();
 		$child_resource = $this->resource->$child_type;
@@ -25,7 +25,7 @@ class ColoCrossing_Resource_Object extends ColoCrossing_Object
 		return $child_resource->findAll($parent_id, $options);
 	}
 
-	public function getResourceChildObject($child_type, $child_id)
+	protected function getResourceChildObject($child_type, $child_id)
 	{
 		if(empty($child_id) || !is_numeric($child_id))
 		{
