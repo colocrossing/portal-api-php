24d00b8490337f172e60c0ff37a152b1daf8c473
Matt Andre <mandre@colocrossing.com>
2014-09-04 15:34:43 -0400

    Change Token in Examples to Be Generic and Remove SSL Ignore Flags

--- examples/remove_null_routes.php
+++ examples/remove_null_routes.php
@@ -3,8 +3,7 @@
 require_once('../src/ColoCrossing.php');
 
 $colocrossing_client = new ColoCrossing_Client();
-$colocrossing_client->setAPIToken('eb3ac813fd85acd1a386c46cd84e39c6bf263c35');
-$colocrossing_client->setOption('ssl_verify', false);
+$colocrossing_client->setAPIToken('YOUR_API_TOKEN');
 
 ?>
 
